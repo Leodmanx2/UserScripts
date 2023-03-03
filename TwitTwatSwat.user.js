4fba39ce72f8c44c02bbd9e44665d88d1529e9ce
Chris MacLeod <chris@chris-macleod.ca>
2023-03-03 18:34:30 -0500

    Add icon, mark override as important

--- TwitTwatSwat.user.js
+++ TwitTwatSwat.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         TwitTwatSwat
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @downloadURL  https://bitbucket.org/leodmanx2/userscripts/raw/HEAD/TwitTwatSwat.user.js
 // @description  Hides tweets from bad Twitter users in conjunction with the Bot Sentinel extension
 // @author       Chris MacLeod
 // @match        https://twitter.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=twitter.com
 // @grant        none
 // ==/UserScript==
 
@@ -20,8 +21,9 @@
 "use strict";
 
 const hide_bad_users = () => {
-	document.querySelectorAll(".Disruptive, .Problematic")
-	  .forEach((post) => { post.closest("article").style.display = "none"; });
+	document.querySelectorAll(".Disruptive, .Problematic").forEach((post) => {
+		post.closest("article").style.setProperty("display", "none", "important");
+	});
 };
 
 setInterval(hide_bad_users, 1000);
